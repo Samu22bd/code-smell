0ffd7b0c6b4e0d8c2c622a6245533bceb922848d
Samu22bd <samesg04@gmail.com>
2024-09-25 19:56:51 +0700

    first commit

---
+++ src/Components/Fragments/Content/Body.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import ExplanationText from "../../Elements/Content/ExplanationText";
+import ExplanationImage from "../../Elements/Content/ExplanationImage";
+import Title from "../../Elements/Content/Title";
+import MyCoolCodeBlock from "../../Elements/Content/CodeHolder";
+import ExplanationExample from "../../Elements/Content/ExplanationExample";
+
+const Body = (props) => {
+    const{ id, title, image, paragraph1, paragraph2, paragraph3, code1, code2, source } = props;
+
+    return(
+        <div>
+            <Title ts={title}/>
+            <ExplanationImage image={image} />
+            <ExplanationText ets={paragraph1} title={title}/>
+            <ExplanationText ets={paragraph2} title={title}/>
+            <ExplanationExample title={title} source={source}/>
+            <MyCoolCodeBlock condition="Before" code={code1} language="java" showLineNumbers="true" />
+            <MyCoolCodeBlock condition="After" code={code2} language="java" showLineNumbers="true" />
+            <div className="flex-col justify-center text-sm text-justify mt-2 md:text-lg">
+                <div id={id}>
+                    {paragraph3}
+                </div>
+            </div>
+        </div>      
+    );
+};
+
+export default Body;
---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import HomePage from './pages/home.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import ChangePreventersPage from './pages/cpreventers.jsx'
+import DispensablesPage from './pages/dispensables.jsx'
+import ErrorPage from './pages/404.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>,
+    errorElement: <ErrorPage/>
+  },
+  {
+    path: "/ChangePreventers",
+    element: <ChangePreventersPage/>
+  },
+  {
+    path: "/Dispensables",
+    element: <DispensablesPage/>
+  },
+  {
+    path: "/Error",
+    element: <ErrorPage/>
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
